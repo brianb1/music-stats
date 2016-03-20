60eb1bce06793ebdf8df3a5ffde6c799bcd0b627
James Vaughan <james@jamesbvaughan.com>
2016-03-19 21:01:42 -0700

    added helper for data stuff

--- helper.js
+++ helper.js
@@ -13,23 +13,49 @@ const lf = lastfm({
 	apiKey: process.env.LASTFM_KEY
 });
 
+let counter = 0;
+
 // Functions
 function printTracks(page, nTracks) {
 	lf.getRecentTracks(nTracks, page, tracks => {
-		console.log(tracks);
 		let trackList = tracks.slice(1).map(track => {
 			return {
 				artist: track.artist['#text'],
 				title: track.name,
+				mbid: track.mbid,
 				album: track.album['#text'],
-				cover: track.image[track.image.length - 1]['#text'],
-				icon: track.image[1]['#text'],
-				url: track.url
+				date: track.date['#text']
 			};
 		});
-		// console.log(trackList);
+		console.error(counter++, "/ 379");
+		console.log(trackList);
+	});
+}
+
+function getCharts() {
+	lf.getWeeklyChartList(weeks => {
+		for (var num in weeks) {
+			lf.getWeeklyTrackChart(weeks, num, (tracks, weekNum) => {
+				console.log(tracks.map(track => {
+					return {
+						artist: track.artist['#text'],
+						title: track.name,
+						album: track.album ? track.album['#text'] : '',
+						cover: track.image[track.image.length - 1]['#text'],
+						icon: track.image[1]['#text'],
+						url: track.url
+					};
+				}));
+			});
+		}
 	});
 }
 
 // Script
-printTracks(1, 5);
+for (var i = 1; i <= 379; i++) {
+	printTracks(i, 200);
+}
+// for (var i = 1; i <= 2; i++) {
+// 	printTracks(i, 3);
+// }
+// getCharts();
